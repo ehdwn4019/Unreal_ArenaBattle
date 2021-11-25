8d4c93277d2637732708a0abf94b2cf9aab4875e
ehdwn4019 <ehdwn7531@naver.com>
2021-11-25 14:14:20 +0900

    TEMP

--- ArenaBattle/Source/ArenaBattleEditor.Target.cs
+++ ArenaBattle/Source/ArenaBattleEditor.Target.cs
@@ -3,12 +3,12 @@
 using UnrealBuildTool;
 using System.Collections.Generic;
 
-public class ArenaBattleEditorTarget : TargetRules
+public class ArenaBattleEditorTarget : TargetRules 
 {
 	public ArenaBattleEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 
-		ExtraModuleNames.AddRange( new string[] { "ArenaBattle" , "ArenaBattleSetting"} );
+		ExtraModuleNames.AddRange( new string[] { "ArenaBattle" , "ArenaBattleSetting" } );
 	}
 }
